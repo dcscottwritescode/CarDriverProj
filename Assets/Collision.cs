1e55d50c3e63b21697ef7645155e4ddfd20ea96e
DC Scott <dcscottwritescode@gmail.com>
2022-01-08 12:58:26 -0800

    Removed Delivery script. Fused with Collision.

--- Assets/Collision.cs
+++ Assets/Collision.cs
@@ -8,4 +8,17 @@ public class Collision : MonoBehaviour
     {
         Debug.Log("Your boss won't be happy about that...");
     }
+
+    void OnTriggerEnter2D(Collider2D other) 
+    {
+        if (other.tag == "Package")
+        {
+              Debug.Log("You have picked up the package! Deliver it to the destination."); 
+        }
+
+        if (other.tag == "Customer")
+        {
+            Debug.Log("You have succesfully delivered the package! Pick up the next one.");
+        }
+    }    
 }
