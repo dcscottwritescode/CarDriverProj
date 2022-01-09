8f503c7adef212186b4403d3b09376153d97303c
DC Scott <dcscottwritescode@gmail.com>
2022-01-08 16:23:11 -0800

    Added new Color32. Car changes color w/ packages.

---
+++ Assets/Delivery.cs
@@ -0,0 +1,44 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Delivery : MonoBehaviour
+{
+    [SerializeField] Color32 packagePickedUp = new Color32 (1, 1, 1, 1);
+    [SerializeField] Color32 packageNotPickedUp = new Color32 (1, 1, 1, 1);
+
+    [SerializeField] float destroyDelay = 1.0f;
+    bool hasPackage;
+
+    SpriteRenderer spriteRenderer;
+
+    void Start() 
+    {
+        spriteRenderer = GetComponent<SpriteRenderer>();    
+    }
+
+    void OnCollisionEnter2D(Collision2D other) 
+    {
+        Debug.Log("Your boss won't be happy about that...");
+    }
+
+    void OnTriggerEnter2D(Collider2D other) 
+    {
+        if (other.tag == "Package" && !hasPackage)
+        {
+            Debug.Log("You have picked up the package! Deliver it to the destination.");
+            hasPackage = true;
+            spriteRenderer.color = packagePickedUp;
+            Destroy(other.gameObject, destroyDelay); 
+        }
+
+
+        if (other.tag == "Customer" && hasPackage)
+        {
+            Debug.Log("You have succesfully delivered the package! Pick up the next one.");
+            hasPackage = false;
+            spriteRenderer.color = packageNotPickedUp;
+        }
+
+    }    
+}
